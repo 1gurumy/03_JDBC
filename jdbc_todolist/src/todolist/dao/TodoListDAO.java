636838c9e53cb33175d9ce7f1eca089e95ebb3b2
1gurumy <a01048406924@gmail.com>
2025-03-28 17:55:33 +0900

    s

--- jdbc_todolist/src/todolist/dao/TodoListDAO.java
+++ jdbc_todolist/src/todolist/dao/TodoListDAO.java
@@ -37,6 +37,10 @@ public class TodoListDAO {
 			
 			result = pstmt.executeUpdate();
 			
+			if(member.getMemberId().equals(rs.getString("MEMBER_ID")) {
+				
+			}
+			
 		} finally {
 			close(pstmt);
 		}
